bc4d3c5b4ab3e9624d0122fc1e43f58fa7a90556
kroegerama <1519044+kroegerama@users.noreply.github.com>
2021-11-03 21:08:56 +0100

    update libraries * activity = "1.4.0" * browser = "1.4.0" * core = "1.7.0" * lifecycle = "2.4.0" * kotlinxDateTime = "0.3.1" * hilt = "2.40"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,18 +1,18 @@
 object V {
 
     object AndroidX {
-        val activity = "1.3.1"
+        val activity = "1.4.0"
         val appcompat = "1.3.1"
         val autofill = "1.1.0"
         val biometric = "1.1.0"
-        val browser = "1.3.0"
+        val browser = "1.4.0"
         val constraintlayout = "2.1.1"
-        val core = "1.6.0"
+        val core = "1.7.0"
         val datastore = "1.0.0"
         val exif = "1.3.3"
         val fragment = "1.3.6"
         val hilt = "1.0.0"
-        val lifecycle = "2.4.0-rc01"
+        val lifecycle = "2.4.0"
         val navigation = "2.3.5"
         val paging = "3.0.1"
         val preference = "1.1.1"
@@ -26,11 +26,11 @@ object V {
         val kotlin = "1.5.31"
         val kotlinxCoroutines = "1.5.2"
         val kotlinxSerialization = "1.3.0"
-        val kotlinxDateTime = "0.3.0"
+        val kotlinxDateTime = "0.3.1"
 
         val desugar = "1.1.5"
 
-        val hilt = "2.39.1"
+        val hilt = "2.40"
 
         val moshi = "1.12.0"
         val okhttp = "4.9.2"
