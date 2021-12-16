63d3b28059bebd5fc81be89a4a4f9bebdd47b879
kroegerama <1519044+kroegerama@users.noreply.github.com>
2021-12-16 22:31:37 +0100

    update libraries * appcompat = "1.4.0" * constraintlayout = "2.1.2" * fragment = "1.4.0" * paging = "3.1.0" * room = "2.4.0" * work = "2.7.1" * kotlin = "1.6.10" * hilt = "2.40.5" * moshi = "1.13.0" * okhttp = "4.9.3" * androidGradle = "7.0.4"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,38 +2,38 @@ object V {
 
     object AndroidX {
         val activity = "1.4.0"
-        val appcompat = "1.3.1"
+        val appcompat = "1.4.0"
         val autofill = "1.1.0"
         val biometric = "1.1.0"
         val browser = "1.4.0"
-        val constraintlayout = "2.1.1"
+        val constraintlayout = "2.1.2"
         val core = "1.7.0"
         val datastore = "1.0.0"
         val exif = "1.3.3"
-        val fragment = "1.3.6"
+        val fragment = "1.4.0"
         val hilt = "1.0.0"
         val lifecycle = "2.4.0"
         val navigation = "2.3.5"
-        val paging = "3.0.1"
+        val paging = "3.1.0"
         val preference = "1.1.1"
         val recycler = "1.2.1"
-        val room = "2.3.0"
+        val room = "2.4.0"
         val swiperefresh = "1.1.0"
-        val work = "2.7.0"
+        val work = "2.7.1"
     }
 
     object Base {
-        val kotlin = "1.6.0"
+        val kotlin = "1.6.10"
         val kotlinxCoroutines = "1.5.2"
         val kotlinxSerialization = "1.3.1"
         val kotlinxDateTime = "0.3.1"
 
         val desugar = "1.1.5"
 
-        val hilt = "2.40.1"
+        val hilt = "2.40.5"
 
-        val moshi = "1.12.0"
-        val okhttp = "4.9.2"
+        val moshi = "1.13.0"
+        val okhttp = "4.9.3"
         val retrofit = "2.9.0"
 
         val material = "1.4.0"
@@ -46,7 +46,7 @@ object V {
     }
 
     object Plugins {
-        val androidGradle = "7.0.3"
+        val androidGradle = "7.0.4"
     }
 
 }
