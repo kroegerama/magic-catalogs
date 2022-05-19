7d62643e071cc33ad2f327704d30b9288e8abd28
kroegerama <1519044+kroegerama@users.noreply.github.com>
2022-05-19 11:05:26 +0200

    update libraries * navigation = "2.4.2" * kotlinxSerialization = "1.3.3" * hilt = "2.42" * material = "1.6.0" * coil = "2.1.0"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -13,7 +13,7 @@ object V {
         val fragment = "1.4.1"
         val hilt = "1.0.0"
         val lifecycle = "2.4.1"
-        val navigation = "2.4.1"
+        val navigation = "2.4.2"
         val paging = "3.1.1"
         val preference = "1.2.0"
         val recycler = "1.2.1"
@@ -25,28 +25,28 @@ object V {
     object Base {
         val kotlin = "1.6.21"
         val kotlinxCoroutines = "1.6.1"
-        val kotlinxSerialization = "1.3.2"
+        val kotlinxSerialization = "1.3.3"
         val kotlinxDateTime = "0.3.2"
 
         val desugar = "1.1.5"
 
-        val hilt = "2.41"
+        val hilt = "2.42"
 
         val moshi = "1.13.0"
         val okhttp = "4.9.3"
         val retrofit = "2.9.0"
 
-        val material = "1.4.0"
+        val material = "1.6.0"
 
         val timber = "5.0.1"
         val logcat = "0.1"
 
-        val coil = "1.4.0"
+        val coil = "2.1.0"
         val chucker = "3.5.2"
     }
 
     object Plugins {
-        val androidGradle = "7.1.3"
+        val androidGradle = "7.2.0"
     }
 
 }
