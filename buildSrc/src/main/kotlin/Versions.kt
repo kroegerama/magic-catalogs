40ae2582aa53e496c82a6480e67ad95938ba42c5
kroegerama <1519044+kroegerama@users.noreply.github.com>
2022-06-30 21:02:51 +0200

    update libraries * activity = "1.5.0" * fragment = "1.5.0" * lifecycle = "2.5.0" * navigation = "2.5.0" * kotlin = "1.7.0" * kotlinxCoroutines = "1.6.3" * kotlinxDateTime = "0.4.0" * desugar = "1.1.6" * okhttp = "4.10.0"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object V {
 
     object AndroidX {
-        val activity = "1.4.0"
+        val activity = "1.5.0"
         val appcompat = "1.4.2"
         val autofill = "1.1.0"
         val biometric = "1.1.0"
@@ -10,10 +10,10 @@ object V {
         val core = "1.8.0"
         val datastore = "1.0.0"
         val exif = "1.3.3"
-        val fragment = "1.4.1"
+        val fragment = "1.5.0"
         val hilt = "1.0.0"
-        val lifecycle = "2.4.1"
-        val navigation = "2.4.2"
+        val lifecycle = "2.5.0"
+        val navigation = "2.5.0"
         val paging = "3.1.1"
         val preference = "1.2.0"
         val recycler = "1.2.1"
@@ -23,17 +23,17 @@ object V {
     }
 
     object Base {
-        val kotlin = "1.6.21"
-        val kotlinxCoroutines = "1.6.2"
+        val kotlin = "1.7.0"
+        val kotlinxCoroutines = "1.6.3"
         val kotlinxSerialization = "1.3.3"
-        val kotlinxDateTime = "0.3.2"
+        val kotlinxDateTime = "0.4.0"
 
-        val desugar = "1.1.5"
+        val desugar = "1.1.6"
 
         val hilt = "2.42"
 
         val moshi = "1.13.0"
-        val okhttp = "4.9.3"
+        val okhttp = "4.10.0"
         val retrofit = "2.9.0"
 
         val material = "1.6.1"
