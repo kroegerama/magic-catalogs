1acbded73fbf1c71d50b4750de0e531204ddf7bd
kroegerama <1519044+kroegerama@users.noreply.github.com>
2022-07-28 20:39:13 +0200

    update libraries * activity = "1.5.1" * fragment = "1.5.1" * lifecycle = "2.5.1" * navigation = "2.5.1" * room = "2.4.3" * hilt = "2.43"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object V {
 
     object AndroidX {
-        val activity = "1.5.0"
+        val activity = "1.5.1"
         val appcompat = "1.4.2"
         val autofill = "1.1.0"
         val biometric = "1.1.0"
@@ -10,14 +10,14 @@ object V {
         val core = "1.8.0"
         val datastore = "1.0.0"
         val exif = "1.3.3"
-        val fragment = "1.5.0"
+        val fragment = "1.5.1"
         val hilt = "1.0.0"
-        val lifecycle = "2.5.0"
-        val navigation = "2.5.0"
+        val lifecycle = "2.5.1"
+        val navigation = "2.5.1"
         val paging = "3.1.1"
         val preference = "1.2.0"
         val recycler = "1.2.1"
-        val room = "2.4.2"
+        val room = "2.4.3"
         val swiperefresh = "1.1.0"
         val work = "2.7.1"
     }
@@ -30,7 +30,7 @@ object V {
 
         val desugar = "1.1.6"
 
-        val hilt = "2.42"
+        val hilt = "2.43"
 
         val moshi = "1.13.0"
         val okhttp = "4.10.0"
