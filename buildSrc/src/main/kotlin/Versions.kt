b725320146cf0d4cbe6187c3c45ea9c3934d4e80
kroegerama <1519044+kroegerama@users.noreply.github.com>
2022-11-18 13:27:31 +0100

    update libraries * activity = "1.6.1" * exif = "1.3.5" * fragment = "1.5.4" * navigation = "2.5.3" * kotlin = "1.7.21" * kotlinxSerialization = "1.4.1" * hilt = "2.44.2" * coil = "2.2.2" * androidGradle = "7.3.1"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object V {
 
     object AndroidX {
-        val activity = "1.6.0"
+        val activity = "1.6.1"
         val appcompat = "1.5.1"
         val autofill = "1.1.0"
         val biometric = "1.1.0"
@@ -9,11 +9,11 @@ object V {
         val constraintlayout = "2.1.4"
         val core = "1.9.0"
         val datastore = "1.0.0"
-        val exif = "1.3.3"
-        val fragment = "1.5.3"
+        val exif = "1.3.5"
+        val fragment = "1.5.4"
         val hilt = "1.0.0"
         val lifecycle = "2.5.1"
-        val navigation = "2.5.2"
+        val navigation = "2.5.3"
         val paging = "3.1.1"
         val preference = "1.2.0"
         val recycler = "1.2.1"
@@ -23,30 +23,30 @@ object V {
     }
 
     object Base {
-        val kotlin = "1.7.10"
+        val kotlin = "1.7.21"
         val kotlinxCoroutines = "1.6.4"
-        val kotlinxSerialization = "1.4.0"
+        val kotlinxSerialization = "1.4.1"
         val kotlinxDateTime = "0.4.0"
 
         val desugar = "1.2.2"
 
-        val hilt = "2.44"
+        val hilt = "2.44.2"
 
         val moshi = "1.14.0"
         val okhttp = "4.10.0"
         val retrofit = "2.9.0"
 
-        val material = "1.6.1"
+        val material = "1.6.1" // 1.7.0
 
         val timber = "5.0.1"
         val logcat = "0.1"
 
-        val coil = "2.2.1"
+        val coil = "2.2.2"
         val chucker = "3.5.2"
     }
 
     object Plugins {
-        val androidGradle = "7.3.0"
+        val androidGradle = "7.3.1"
     }
 
 }
