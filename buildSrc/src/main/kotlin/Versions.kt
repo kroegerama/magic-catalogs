f779d2b4d62ad5de69095bfd6737aba318d19ad5
kroegerama <1519044+kroegerama@users.noreply.github.com>
2023-07-28 10:28:19 +0200

    update libraries * fragment = "1.6.1" * paging = "3.2.0" * preference = "1.2.1" * recycler = "1.3.1" * kotlin = "1.9.0" * kotlinxCoroutines = "1.7.3" * hilt = "2.47" * androidGradle = "8.1.0"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,27 +10,27 @@ object V {
         val core = "1.10.1"
         val datastore = "1.0.0"
         val exif = "1.3.6"
-        val fragment = "1.6.0"
+        val fragment = "1.6.1"
         val hilt = "1.0.0"
         val lifecycle = "2.6.1"
         val navigation = "2.6.0"
-        val paging = "3.1.1"
-        val preference = "1.2.0"
-        val recycler = "1.3.0"
+        val paging = "3.2.0"
+        val preference = "1.2.1"
+        val recycler = "1.3.1"
         val room = "2.5.2"
         val swiperefresh = "1.1.0"
         val work = "2.8.1"
     }
 
     object Base {
-        val kotlin = "1.8.21"
-        val kotlinxCoroutines = "1.7.1"
+        val kotlin = "1.9.0"
+        val kotlinxCoroutines = "1.7.3"
         val kotlinxSerialization = "1.5.1"
         val kotlinxDateTime = "0.4.0"
 
         val desugar = "2.0.3"
 
-        val hilt = "2.46.1"
+        val hilt = "2.47"
 
         val moshi = "1.15.0"
         val okhttp = "4.10.0"
@@ -46,7 +46,7 @@ object V {
     }
 
     object Plugins {
-        val androidGradle = "8.0.2"
+        val androidGradle = "8.1.0"
     }
 
 }
