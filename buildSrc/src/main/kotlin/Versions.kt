3df89a31a019a2de59ff477957482d0f7a83c978
kroegerama <1519044+kroegerama@users.noreply.github.com>
2023-09-09 14:49:58 +0200

    add libraries / plugins * camerax = "1.2.3" * core = "1.12.0" * lifecycle = "2.6.2" * navigation = "2.7.2" * paging = "3.2.1" * kotlinxDateTime = "0.4.1" * dagger = "2.48" * arrow = "1.2.1" * grgit = "5.2.0"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,22 +4,22 @@ object AndroidX {
     val autofill = "1.1.0"
     val biometric = "1.1.0"
     val browser = "1.6.0"
+    val camerax = "1.2.3"
     val constraintlayout = "2.1.4"
-    val core = "1.10.1"
+    val core = "1.12.0"
     val datastore = "1.0.0"
     val exif = "1.3.6"
     val fragment = "1.6.1"
     val hilt = "1.0.0"
-    val lifecycle = "2.6.1"
-    val navigation = "2.7.1"
-    val paging = "3.2.0"
+    val lifecycle = "2.6.2"
+    val navigation = "2.7.2"
+    val paging = "3.2.1"
     val preference = "1.2.1"
     val recyclerview = "1.3.1"
     val recyclerviewSelection = "1.1.0"
     val room = "2.5.2"
     val swiperefresh = "1.1.0"
     val work = "2.8.1"
-    val camerax = "1.2.3"
 }
 
 object Magic {
@@ -27,11 +27,11 @@ object Magic {
     val kotlinKsp = "1.9.10-1.0.13"
     val kotlinxCoroutines = "1.7.3"
     val kotlinxSerialization = "1.6.0"
-    val kotlinxDateTime = "0.4.0"
+    val kotlinxDateTime = "0.4.1"
 
     val desugar = "2.0.3"
 
-    val dagger = "2.47"
+    val dagger = "2.48"
 
     val moshi = "1.15.0"
     val okhttp = "4.11.0"
@@ -45,11 +45,11 @@ object Magic {
     val coil = "2.4.0"
     val chucker = "3.5.2"
 
-    val arrow = "1.2.0"
+    val arrow = "1.2.1"
     val zxing = "3.5.2"
 
     val spotless = "6.19.0"
-    val grgit = "4.1.0"
+    val grgit = "5.2.0"
 
     val androidGradle = "8.1.1"
 }
