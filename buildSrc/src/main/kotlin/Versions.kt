9f6eaf218a319cd96fb305f595758c09446b6ec1
kroegerama <1519044+kroegerama@users.noreply.github.com>
2023-11-14 11:18:00 +0100

    update/add libraries * fragment = "1.6.2" * hilt = "1.1.0" * navigation = "2.7.5" * kotlin = "1.9.20" * kotlinKsp = "1.9.20-1.0.14" * desugar = "2.0.4" * coil = "2.5.0" * grgit = "5.2.1" * androidGradle = "8.1.3"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,10 +9,10 @@ object AndroidX {
     val core = "1.12.0"
     val datastore = "1.0.0"
     val exif = "1.3.6"
-    val fragment = "1.6.1"
-    val hilt = "1.0.0"
+    val fragment = "1.6.2"
+    val hilt = "1.1.0"
     val lifecycle = "2.6.2"
-    val navigation = "2.7.4"
+    val navigation = "2.7.5"
     val paging = "3.2.1"
     val preference = "1.2.1"
     val recyclerview = "1.3.2"
@@ -24,13 +24,13 @@ object AndroidX {
 }
 
 object Magic {
-    val kotlin = "1.9.10"
-    val kotlinKsp = "1.9.10-1.0.13"
+    val kotlin = "1.9.20"
+    val kotlinKsp = "1.9.20-1.0.14"
     val kotlinxCoroutines = "1.7.3"
     val kotlinxSerialization = "1.6.0"
     val kotlinxDateTime = "0.4.1"
 
-    val desugar = "2.0.3"
+    val desugar = "2.0.4"
 
     val dagger = "2.48.1"
 
@@ -43,14 +43,14 @@ object Magic {
     val timber = "5.0.1"
     val logcat = "0.1"
 
-    val coil = "2.4.0"
+    val coil = "2.5.0"
     val chucker = "4.0.0"
 
     val arrow = "1.2.1"
     val zxing = "3.5.2"
 
     val spotless = "6.19.0"
-    val grgit = "5.2.0"
+    val grgit = "5.2.1"
 
-    val androidGradle = "8.1.2"
+    val androidGradle = "8.1.3"
 }
