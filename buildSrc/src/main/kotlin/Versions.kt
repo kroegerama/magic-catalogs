82b83e202a17c723006373571b4fc7dd8ed04211
kroegerama <1519044+kroegerama@users.noreply.github.com>
2024-02-29 13:45:46 +0100

    update libraries * hilt = "1.2.0" * kotlinKsp = "1.9.22-1.0.18" * dagger = "2.51" * coil = "2.6.0" * arrow = "1.2.3" * grgit = "5.2.2"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,7 +10,7 @@ object AndroidX {
     val datastore = "1.0.0"
     val exif = "1.3.7"
     val fragment = "1.6.2"
-    val hilt = "1.1.0"
+    val hilt = "1.2.0"
     val lifecycle = "2.7.0"
     val navigation = "2.7.7"
     val paging = "3.2.1"
@@ -25,14 +25,14 @@ object AndroidX {
 
 object Magic {
     val kotlin = "1.9.22"
-    val kotlinKsp = "1.9.22-1.0.17"
+    val kotlinKsp = "1.9.22-1.0.18"
     val kotlinxCoroutines = "1.8.0"
     val kotlinxSerialization = "1.6.3"
     val kotlinxDateTime = "0.5.0"
 
     val desugar = "2.0.4"
 
-    val dagger = "2.50"
+    val dagger = "2.51"
 
     val moshi = "1.15.1"
     val okhttp = "4.12.0"
@@ -43,14 +43,14 @@ object Magic {
     val timber = "5.0.1"
     val logcat = "0.1"
 
-    val coil = "2.5.0"
+    val coil = "2.6.0"
     val chucker = "4.0.0"
 
-    val arrow = "1.2.1"
+    val arrow = "1.2.3"
     val zxing = "3.5.3"
 
     val spotless = "6.19.0"
-    val grgit = "5.2.1"
+    val grgit = "5.2.2"
 
     val androidGradle = "8.2.2"
 }
