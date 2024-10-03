24403cd6382e2e5ce019092cd3a3b2d8e3840353
kroegerama <1519044+kroegerama@users.noreply.github.com>
2024-10-03 11:54:35 +0200

    update libraries * fragment = "1.8.4" * lifecycle = "2.8.6" * navigation = "2.8.2" * startup = "1.2.0" * composeBom = "2024.09.03" * kotlinxSerialization = "1.7.3" * androidGradle = "8.7.0"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,29 +10,29 @@ object AndroidX {
     val datastore = "1.1.1"
     val documentfile = "1.0.1"
     val exif = "1.3.7"
-    val fragment = "1.8.3"
+    val fragment = "1.8.4"
     val hilt = "1.2.0"
-    val lifecycle = "2.8.5"
+    val lifecycle = "2.8.6"
     val palette = "1.0.0"
-    val navigation = "2.8.0"
+    val navigation = "2.8.2"
     val paging = "3.3.2"
     val preference = "1.2.1"
     val recyclerview = "1.3.2"
     val recyclerviewSelection = "1.1.0"
     val room = "2.6.1"
-    val startup = "1.1.1"
+    val startup = "1.2.0"
     val swiperefresh = "1.1.0"
     val viewpager2 = "1.1.0"
     val work = "2.9.1"
 
-    val composeBom = "2024.09.01"
+    val composeBom = "2024.09.03"
 }
 
 object Magic {
     val kotlin = "2.0.20"
     val kotlinKsp = "2.0.20-1.0.25"
     val kotlinxCoroutines = "1.9.0"
-    val kotlinxSerialization = "1.7.2"
+    val kotlinxSerialization = "1.7.3"
     val kotlinxDateTime = "0.6.1"
 
     val desugar = "2.1.2"
@@ -57,5 +57,5 @@ object Magic {
     val spotless = "6.25.0"
     val grgit = "5.2.2"
 
-    val androidGradle = "8.6.0"
+    val androidGradle = "8.7.0"
 }
