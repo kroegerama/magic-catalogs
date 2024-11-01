7837917cb2c1077cef34cddf9484da30b63da396
kroegerama <1519044+kroegerama@users.noreply.github.com>
2024-11-01 11:33:42 +0100

    update libraries * camerax = "1.4.0" * constraintlayout = "2.2.0" * core = "1.15.0" * fragment = "1.8.5" * lifecycle = "2.8.7" * work = "2.10.0" * composeBom = "2024.10.01" * kotlinKsp = "2.0.21-1.0.26" * androidGradle = "8.7.2"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,15 +4,15 @@ object AndroidX {
     val autofill = "1.1.0"
     val biometric = "1.1.0"
     val browser = "1.8.0"
-    val camerax = "1.3.4"
-    val constraintlayout = "2.1.4"
-    val core = "1.13.1"
+    val camerax = "1.4.0"
+    val constraintlayout = "2.2.0"
+    val core = "1.15.0"
     val datastore = "1.1.1"
     val documentfile = "1.0.1"
     val exif = "1.3.7"
-    val fragment = "1.8.4"
+    val fragment = "1.8.5"
     val hilt = "1.2.0"
-    val lifecycle = "2.8.6"
+    val lifecycle = "2.8.7"
     val palette = "1.0.0"
     val navigation = "2.8.3"
     val paging = "3.3.2"
@@ -23,14 +23,14 @@ object AndroidX {
     val startup = "1.2.0"
     val swiperefresh = "1.1.0"
     val viewpager2 = "1.1.0"
-    val work = "2.9.1"
+    val work = "2.10.0"
 
-    val composeBom = "2024.10.00"
+    val composeBom = "2024.10.01"
 }
 
 object Magic {
     val kotlin = "2.0.21"
-    val kotlinKsp = "2.0.21-1.0.25"
+    val kotlinKsp = "2.0.21-1.0.26"
     val kotlinxCoroutines = "1.9.0"
     val kotlinxSerialization = "1.7.3"
     val kotlinxDateTime = "0.6.1"
@@ -57,5 +57,5 @@ object Magic {
     val spotless = "6.25.0"
     val grgit = "5.3.0"
 
-    val androidGradle = "8.7.1"
+    val androidGradle = "8.7.2"
 }
