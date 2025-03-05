6b6e549a7a1ec245d7617c5892753a0a3c3c7303
kroegerama <1519044+kroegerama@users.noreply.github.com>
2025-03-05 20:57:56 +0100

    update libraries * activity = "1.10.1" * constraintlayout = "2.2.1" * datastore = "1.1.3" * exif = "1.4.0" * navigation = "2.8.8" * kotlinKsp = "2.1.10-1.0.31" * desugar = "2.1.5" * spotless = "7.0.2" * androidGradle = "8.9.0"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,19 +1,19 @@
 object AndroidX {
-    val activity = "1.10.0"
+    val activity = "1.10.1"
     val appcompat = "1.7.0"
     val autofill = "1.1.0"
     val biometric = "1.1.0"
     val browser = "1.8.0"
     val camerax = "1.4.1"
-    val constraintlayout = "2.2.0"
+    val constraintlayout = "2.2.1"
     val core = "1.15.0"
-    val datastore = "1.1.2"
+    val datastore = "1.1.3"
     val documentfile = "1.0.1"
-    val exif = "1.3.7"
+    val exif = "1.4.0"
     val fragment = "1.8.6"
     val hilt = "1.2.0"
     val lifecycle = "2.8.7"
-    val navigation = "2.8.7"
+    val navigation = "2.8.8"
     val paging = "3.3.6"
     val palette = "1.0.0"
     val preference = "1.2.1"
@@ -30,13 +30,13 @@ object AndroidX {
 
 object Magic {
     val kotlin = "2.1.10"
-    val kotlinKsp = "2.1.10-1.0.30"
+    val kotlinKsp = "2.1.10-1.0.31"
     val kotlinxCoroutines = "1.10.1"
     val kotlinxSerialization = "1.8.0"
     val kotlinxDateTime = "0.6.2"
     val kotlinxImmutableCollections = "0.3.8"
 
-    val desugar = "2.1.4"
+    val desugar = "2.1.5"
 
     val dagger = "2.55"
 
@@ -57,8 +57,8 @@ object Magic {
     val arrow = "2.0.1"
     val zxing = "3.5.3"
 
-    val spotless = "6.25.0"
+    val spotless = "7.0.2"
     val grgit = "5.3.0"
 
-    val androidGradle = "8.8.1"
+    val androidGradle = "8.9.0"
 }
